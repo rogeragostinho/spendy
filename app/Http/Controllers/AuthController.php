082a8dd792d713b9116ce1b3c1430b15559d40e8
Roger Agostinho <167251142+rogeragostinho@users.noreply.github.com>
2026-04-09 01:51:24 +0100

    feat:: add login funcionality

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,12 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
-
         $validated = $request->validate([
             'name'     => 'required|string|min:3',
             'email'    => 'required|email|unique:users',
@@ -20,6 +20,33 @@ class AuthController extends Controller
 
         $token = $user->createToken('auth_token');
 
-        return response()->json(['token' => $token->plainTextToken], 201);
+        return response()->json([
+            'token' => $token->plainTextToken,
+            'token_type' => 'Bearer',
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials  = $request->validate([
+            'email'    => 'required|email',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'Credenciais inválidas'
+            ], 401);
+        }
+
+        /** @var \App\Models\User $user */
+        $user = Auth::user();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+            'token_type' => 'Bearer',
+        ]);
     }
 }
